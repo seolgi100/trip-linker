375013f0fec7f72f87e9a209d7ea68fe92cc4384
seolgi <seolgi100k@gmail.com>
2026-05-25 16:27:52 +0900

    init: TripLinker 프로젝트 초기 뼈대 세팅

---
+++ src/main/java/idusw/sbb/triplinker/TripLinkerApplication.java
@@ -0,0 +1,13 @@
+package idusw.sbb.triplinker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripLinkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripLinkerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/idusw/sbb/triplinker/TripLinkerApplicationTests.java
@@ -0,0 +1,13 @@
+package idusw.sbb.triplinker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripLinkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
